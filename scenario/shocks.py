0a7c21f10957a328127696936b4bb960404c3a9d
montrixdev <montrix@naver.com>
2020-12-23 20:09:18 +0900

    working...

--- scenario/shocks.py
+++ scenario/shocks.py
@@ -1,4 +1,5 @@
 # shock
+import mxdevtool as mx
 import mxdevtool.xenarix as xen
 
 class GenerateHelper:
@@ -39,3 +40,58 @@ for data in marketdata:
 
 
 # 완성되면 이건 여러가지 시나리오등을 한꺼번에 생성함
+
+# -----------------------------------------------------------------
+
+mrk_d = mx.MarketData()
+
+scen1 = xen.Scenario(name, models, calcs, corr, timegrid, rsg, isMomentMatching)
+scen1.toJson()
+
+def toJson(self):
+    json = dict()
+
+    json['models'] = [m.toJson() for m in models]
+    json['calcs'] = [c.toJson() for c in calcs]
+
+    return json
+
+
+sb = xen.ScenarioBuilder(json)
+
+
+# 하고 싶은게 뭐냐면.
+
+# 만들어진 시나리오를 그냥 또는 묶어서 저장하고 싶다.
+
+xm = xen.XenarixManager(config)
+
+## 그냥
+scen1 = xen.Scenario(name, models, calcs, corr, timegrid, rsg, isMomentMatching)
+xm.save(name, scen1) # save 내부에서 json 을 이용하여, serialize 한다음에 저장함. 밖에서는 알필요 없음.
+
+## 묶은거
+scenset1 = xen.ScenarioSet(name, [scen1, scen2])
+xm.save(name, scenset1)
+
+# 저장된 시나리오를 불러오고 싶다.
+scen1 = xm.load(name) # load 내부에서 json을 불러와서 다시 생성해냄.
+
+# 저장된 시나리오를 보고 싶다.
+xm.get_list(filter='test')
+
+
+# -----------------------------------
+# MarketData랑 연동 된 모델(Shock같은거 처리나, mrk_d만 업데이트 해서 쓰는 용도)은 builder를 이용함.
+
+sb = xen.ScenarioBuilder()
+
+curve = mrk_d.get_curve('name')
+curve.spread(0.0001)
+
+gbm = xen.GBM(x0=100, rfCurve='irskrw', div='irskrw')
+sb.add_model(gbm)
+
+
+
+
