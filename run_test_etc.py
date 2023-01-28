69c654191b208f65d753cd86306ce495989d5a10
minikie <math.ansang@gmail.com>
2023-01-29 00:11:04 +0900

    working...

--- run_test_etc.py
+++ run_test_etc.py
@@ -9,28 +9,27 @@ def timegrid_test():
     maxYear = 10
 
     # TimeGrid
-    timeGrid1 = mx.TimeEqualGrid(ref_date, 3, 365)
-    timeGrid2 = mx.TimeArrayGrid(ref_date, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
-    timeGrid3 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='endofmonth')
-    timeGrid4 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='day')
-    timeGrid4 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='week')
-    timeGrid5 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='month', frequency_day=10)
-    timeGrid6 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='quarter', frequency_day=10)
-    timeGrid7 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='semiannual', frequency_day=10)
-    timeGrid8 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='annual', frequency_month=8, frequency_day=10)
-    timeGrid9 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='firstofmonth')
-    timeGrid10 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='firstofquarter')
-    timeGrid11 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='firstofsemiannual')
-    timeGrid12 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='firstofannual')
-    timeGrid13 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='endofmonth')
-    timeGrid14 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='endofquarter')
-    timeGrid15 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='endofsemiannual')
-    timeGrid16 = mx.TimeGrid(refDate=ref_date, maxYear=maxYear, frequency_type='endofannual')
+    timegrid1  = mx.TimeDateGrid_Equal(refDate=ref_date, maxYear=3, nPerYear=365)
+    timegrid2  = mx.TimeDateGrid_Times(refDate=ref_date, times=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+    timegrid3  = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='day')
+    timegrid4  = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='week')
+    timegrid5  = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='month', frequency_day=10)
+    timegrid6  = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='quarter', frequency_day=10)
+    timegrid7  = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='semiannual', frequency_day=10)
+    timegrid8  = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='annual', frequency_month=8, frequency_day=10)
+    timegrid9  = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='firstofmonth')
+    timegrid10 = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='firstofquarter')
+    timegrid11 = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='firstofsemiannual')
+    timegrid12 = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='firstofannual')
+    timegrid13 = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='endofmonth')
+    timegrid14 = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='endofquarter')
+    timegrid15 = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='endofsemiannual')
+    timegrid16 = mx.TimeDateGrid_Custom(refDate=ref_date, maxYear=maxYear, frequency_type='endofannual')
 
-    timeGrids = [ timeGrid1, timeGrid2, timeGrid3, timeGrid4, timeGrid5, timeGrid6, timeGrid7, timeGrid8, timeGrid9, timeGrid10, 
-                  timeGrid11, timeGrid12, timeGrid13, timeGrid14, timeGrid15, timeGrid16]
+    timegrids = [ timegrid1, timegrid2, timegrid3, timegrid4, timegrid5, timegrid6, timegrid7, timegrid8, timegrid9, timegrid10, 
+                  timegrid11, timegrid12, timegrid13, timegrid14, timegrid15, timegrid16]
 
-    for tg, i in zip(timeGrids, range(len(timeGrids))):
+    for tg, i in zip(timegrids, range(len(timegrids))):
         print('timeGrid{0} :'.format(i+1), tg.dates()[:10]) 
 
 
